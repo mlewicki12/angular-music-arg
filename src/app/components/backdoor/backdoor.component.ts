810420ef46efcb7fab3e60605ed001eb4c764c4e
Maciej Lewicki <lewickimapl@gmail.com>
2021-01-03 02:30:59 +0100

    expand backdoor to allow for different challenges

--- src/app/components/backdoor/backdoor.component.ts
+++ src/app/components/backdoor/backdoor.component.ts
@@ -8,8 +8,44 @@ import { BindingService } from 'src/app/services/binding.service';
   styleUrls: ['./backdoor.component.scss']
 })
 export class BackdoorComponent implements OnInit {
+  challenges = [
+    {
+      text: 'To initiate backdoor protocol, complete the following number sequence:',
+      success: 'Backdoor protocol initialising',
+      sequence: [7, 28, 112, 448, 1792],
+      answerIndex: 3,
+      answer: '448'
+    },
+    {
+      text: 'Complete the sequence to install packet sniffer:',
+      success: 'Packet sniffer installing',
+      sequence: [3369, 3693, 6933, 9336],
+      answerIndex: 2,
+      answer: '6933'
+    },
+    {
+      text: 'Enter missing pin to install rootkit:',
+      success: 'Rootkit installing',
+      sequence: [144, 233, 377, 610, 987, 1597],
+      answerIndex: 2,
+      answer: '377'
+    },
+    {
+      text: 'Complete password to gain admin privileges:',
+      success: 'Enabling admin privileges',
+      sequence: ['Tokyo', 'Delhi', 'Shanghai', 'São Paulo', 'Mexico City'],
+      answerIndex: 0,
+      answer: 'Tokyo'
+    }
+  ];
+
   answer: string = '*****';
   attempts: number = 0;
+
+  challenge: any;
+  challengeIndex: number = 0;
+  challengeTextPreInput: string;
+  challengeTextPostInput: string;
 
   displayText: string = '0 / 3';
   displayClass: string = 'hide';
@@ -19,20 +55,25 @@ export class BackdoorComponent implements OnInit {
   disabled: boolean = false;
 
   constructor(private binding: BindingService,
-              private router: Router) { }
+              private router: Router) { 
+    this.challenge = this.challenges[this.challengeIndex];
+    this.challengeTextPreInput = this.challenge.sequence.splice(0, this.challenge.answerIndex).join(', ');
+    this.challengeTextPostInput = this.challenge.sequence.splice(1).join(', ');
+  }
 
   ngOnInit(): void {
     this.binding.registerEvent('Enter', () => {
-      if(this.answer.trim() === '448') {
+      if(this.answer.trim() === this.challenge.answer) {
         this.displayClass = 'success';
         this.disabled = true;
 
-        this.loadingText('Backdoor protocol initialising', 3000, '/hack');
+        this.loadingText(this.challenge.success, 3000, '/hack');
       } else {
         this.attempts += 1;
 
         if(this.attempts >= 3) {
           this.disabled = true;
+          this.attempts = 0;
 
           this.loadingText('Attempt to breach detected, purging', 3000, '/login');
         } else {
